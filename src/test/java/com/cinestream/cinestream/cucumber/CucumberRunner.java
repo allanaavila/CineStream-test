e8e12cdf6013436c5d9b363258a6f0a6d2c388ad
Allana <allanac.avila@gmail.com>
2024-11-27 20:03:23 -0300

    ajustando configuracao

---
+++ src/test/java/com/cinestream/cinestream/cucumber/CucumberRunner.java
@@ -0,0 +1,14 @@
+package com.cinestream.cinestream.cucumber;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"html:target/cucumber-reports/Cucumber.html, pretty"},
+        features = "src/test/resources/features"
+)
+public class CucumberRunner {
+
+}
